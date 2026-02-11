dfc15ec13e7e0b791533ca7b175709e2bbdbf45e
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-11 18:27:15 -0500

    auto

--- canon/proof_gate/ProofGate/Invariants.lean
+++ canon/proof_gate/ProofGate/Invariants.lean
@@ -1,6 +1,6 @@
-import LearnGate.State
+ximport ProofGate.State
 
-namespace LearnGate
+namespace ProofGate
 
 /-- Invariants are predicates over State. Currently we just require that a root hash exists. -/
 def Inv (_ : State) : Prop := True
@@ -8,4 +8,4 @@ def Inv (_ : State) : Prop := True
 theorem inv_holds (s : State) : Inv s := by
   exact trivial
 
-end LearnGate
+end ProofGate
