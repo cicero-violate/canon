4369a0f73aca4ef268536f868e6bb4d9950c92df
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-14 02:56:55 -0500

    auto

--- canon/run_seed.sh
+++ canon/run_seed.sh
@@ -1,4 +1,5 @@
 cd /home/cicero-arch-omen/ai_sandbox/canon/canon
+cargo build --bin canon 2>&1 | tail -3
 
 # 1. Ingest canon's own source into an IR
 cargo run --bin canon -- ingest \
@@ -7,9 +8,11 @@ cargo run --bin canon -- ingest \
   --layout-out /home/cicero-arch-omen/ai_sandbox/canon/canon.layout.json
 
 # 2. Bootstrap the capability graph and seed proposal
-cargo run -- bootstrap-graph \
+cargo run --bin canon -- bootstrap-graph \
+  --ir /home/cicero-arch-omen/ai_sandbox/canon/canon.ir.json \
   --graph-out /home/cicero-arch-omen/ai_sandbox/canon/canon.graph.json \
   --proposal-out /home/cicero-arch-omen/ai_sandbox/canon/canon.proposal.json
+
 
 # 2. Start the chromium_messenger daemon (you do this manually)
 ./run_chromium_daemon.sh
