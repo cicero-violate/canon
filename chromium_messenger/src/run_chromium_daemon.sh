eda99f815f12b602f0e7adae8b4085b3fbbc6d0a
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-14 02:13:13 -0500

    auto

--- chromium_messenger/src/run_chromium_daemon.sh
+++ chromium_messenger/src/run_chromium_daemon.sh
@@ -1,1 +1,1 @@
-/home/cicero-arch-omen/ai_sandbox/chatgpt-website/chatgpt_agent/target/debug/chromium_messenger --daemon
+/home/cicero-arch-omen/ai_sandbox/canon/target/debug/chromium_messenger --daemon
