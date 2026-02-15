e83249bde2df8e109973f4a67966543409d4c17e
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-15 00:24:24 -0500

    auto

--- canon/run_seed.sh
+++ canon/run_seed.sh
@@ -10,7 +10,7 @@ cargo run --bin canon -- ingest \
 cargo run --bin canon -- ingest \
   --src /home/cicero-arch-omen/ai_sandbox/canon/memory/memory_engine \ 
   --semantic-out /home/cicero-arch-omen/ai_sandbox/canon/canon_store/memory_engine.ir.json \
-  --layout-out /home/cicero-arch-omen/ai_sandbox/canon/canon_store/memory_engine..layout.json
+  --layout-out /home/cicero-arch-omen/ai_sandbox/canon/canon_store/memory_engine.layout.json
 
 # 2. Bootstrap the capability graph and seed proposal
 # cargo run --bin canon -- bootstrap-graph \
