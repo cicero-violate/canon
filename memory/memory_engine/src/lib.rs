15e2beaa63fd247059405744ff4a706865cc988a
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-15 23:02:28 -0500

    auto

--- memory/memory_engine/src/lib.rs
+++ memory/memory_engine/src/lib.rs
@@ -23,6 +23,4 @@ pub use memory_engine::{
 };
 
 // expose proof types needed by canon
-// pub use crate::proofs::JudgmentProof;
-// pub use primitives::Hash;
 pub use proofs::{JudgmentProof, AdmissionProof, CommitProof, OutcomeProof};
