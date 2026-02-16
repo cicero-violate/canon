a87f65a6998c3450ba4d3fdf3f2d644151b1e057
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-16 03:33:35 -0500

    auto

--- memory/memory_engine/src/tlog/tlog_replay.rs
+++ memory/memory_engine/src/tlog/tlog_replay.rs
@@ -3,6 +3,7 @@ use crate::canonical_state::CanonicalState;
 use crate::page::Page;
 use crate::page::{DeltaAppliable, PageAccess};
 use crate::tlog::TlogManager;
+use crate::hash::gpu::create_gpu_backend;
 use std::path::Path;
 
 pub fn apply_log(pages: &mut [Page], deltas: &[Delta]) {
@@ -15,7 +16,7 @@ pub fn apply_log(pages: &mut [Page], deltas: &[Delta]) {
 
 pub fn replay(path: &Path) -> std::io::Result<CanonicalState> {
     let manager = TlogManager::new(path)?;
-    let mut state = CanonicalState::new_empty();
+    let mut state = CanonicalState::new_empty(create_gpu_backend());
     manager.replay_all(&mut state)?;
     Ok(state)
 }
