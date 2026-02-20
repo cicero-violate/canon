f0f3117d2de59e61cc28fbba52bef1b0104853ae
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-20 10:45:09 -0500

    auto

--- rename/src/mod.rs
+++ rename/src/mod.rs
@@ -12,22 +12,33 @@ pub mod pattern;
 pub mod scope;
 pub mod structured;
 
+pub use crate::alias;
+pub use crate::api;
+pub use crate::attributes;
+pub use crate::core;
+pub use crate::macros;
+pub use crate::module_path;
+pub use crate::occurrence;
+pub use crate::pattern;
+pub use crate::scope;
+pub use crate::structured;
+
 // Re-export main rename functions
 pub use core::{
+    apply_rename, apply_rename_with_map, collect_names, emit_names, run_names, run_rename,
     LineColumn, SpanRange, StructuredEditTracker, SymbolIndexReport, SymbolOccurrence,
-    SymbolRecord, apply_rename, apply_rename_with_map, collect_names, emit_names, run_names,
-    run_rename,
+    SymbolRecord,
 };
 
 pub use alias::{AliasGraph, ImportNode, UseKind, VisibilityScope};
 
 pub use api::{
-    MutationRequest, MutationResult, QueryRequest, QueryResult, UpsertRequest, UpsertResult,
-    execute_mutation_json, execute_query_json, execute_upsert_json,
+    execute_mutation_json, execute_query_json, execute_upsert_json, MutationRequest,
+    MutationResult, QueryRequest, QueryResult, UpsertRequest, UpsertResult,
 };
 
 // C1: Re-export structured pass infrastructure
 pub use structured::{
-    AstEdit, DocAttrPass, StructuredPassRunner, StructuredEditOptions, StructuredPass, UsePathRewritePass,
-    apply_ast_rewrites, ast_render, create_rename_orchestrator,
+    apply_ast_rewrites, ast_render, create_rename_orchestrator, AstEdit, DocAttrPass,
+    StructuredEditOptions, StructuredPass, StructuredPassRunner, UsePathRewritePass,
 };
