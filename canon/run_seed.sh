2c17be063efd1a6bb83beb30306b375524225891
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-20 08:11:41 -0500

    auto

--- canon/run_seed.sh
+++ canon/run_seed.sh
@@ -38,6 +38,12 @@ cargo run -p canon -- materialize \
   --graph-out /workspace/canon_workspace/canon.graph.json \
   --proposal-out /workspace/ai_sandbox/canon_workspace/canon.proposal.json
 
+/workspace/ai_sandbox/canon_workspace/target/debug/canon bootstrap-graph \
+  --ir /workspace/ai_sandbox/canon_workspace/canon.ir.json \
+  --graph-out /workspace/ai_sandbox/canon_workspace/canon.graph.json \
+  --proposal-out /workspace/ai_sandbox/canon_workspace/canon.proposal.json
+
+
 # 2. Start the chromium_messenger daemon (you do this manually)
 # ./run_chromium_daemon.sh
 
