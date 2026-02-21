65eafb6d3d57190e142d1e18ba24cc732bf9aa09
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-21 15:19:41 -0500

    auto

--- rename/cargo_fix_point.sh
+++ rename/cargo_fix_point.sh
@@ -1,3 +1,5 @@
+cd /workspace/ai_sandbox/canon_workspace/rename
+
 rm -f ../.rename/state.tlog ../.rename/state.log 2>/dev/null
 find .. -name "state.tlog" -o -name "state.log" -o -name "*.graph.log" | head -20
 
