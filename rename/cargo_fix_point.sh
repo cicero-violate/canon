a23a249ecefdbafaa39095d8c187559b1e38ac47
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-21 15:18:18 -0500

    auto

--- rename/cargo_fix_point.sh
+++ rename/cargo_fix_point.sh
@@ -1,1 +1,4 @@
+rm -f ../.rename/state.tlog ../.rename/state.log 2>/dev/null
+find .. -name "state.tlog" -o -name "state.log" -o -name "*.graph.log" | head -20
+
 cargo run --example nodeop_movesymbol
