0ef687f2da53ef6ec97447ff6dd56989ecd0f3c0
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-21 04:32:38 -0500

    auto

--- rename/cargo_build_json.sh
+++ rename/cargo_build_json.sh
@@ -1,11 +1,15 @@
 cargo build --message-format=json 2>/dev/null \
 | jq -c 'select(.reason=="compiler-message")
          | .message
-         | select(.level=="error")
-         | {code: (.code.code // "unknown"),
+         | select(.level=="error" or .level=="warning")
+         | {level: .level,
+            code: (.code.code // "unknown"),
             message: .message,
             spans: [.spans[]? | {file: .file_name, line: .line_start, col: .column_start}]}' \
-> build_errors.json
+> build_diagnostics.json
 
-jq -r '.code' build_errors.json | sort | uniq -c | sort -k2
-bat -n build_errors.json 
+jq -r '.level + ":" + .code' build_diagnostics.json \
+| sort | uniq -c | sort -k2
+
+bat -n build_diagnostics.json
+
