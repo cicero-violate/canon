77d7df1ee9570b6dd98b97eb5c42d47ccc8a9746
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-21 19:06:57 -0500

    auto

--- kernel/src/engine_commit.rs
+++ kernel/src/engine_commit.rs
@@ -1,31 +1,16 @@
+use crate::memory_engine::Kernel;
 use crate::{
-    delta::DeltaError, memory_engine::{CommitError, MemoryEngineError},
-    primitives::StateHash, proofs::{AdmissionProof, CommitProof},
+    delta::DeltaError,
+    memory_engine::{CommitError, KernelError},
+    primitives::StateHash,
+    proofs::{AdmissionProof, CommitProof},
 };
-use crate::memory_engine::MemoryEngine;
-impl MemoryEngine {
-    pub fn commit_delta(
-        &self,
-        admission: &AdmissionProof,
-        delta_hash: &StateHash,
-    ) -> Result<CommitProof, MemoryEngineError> {
-        let delta = self
-            .fetch_delta_by_hash(delta_hash)
-            .ok_or(MemoryEngineError::DeltaNotFound)?;
-        self.state
-            .write()
-            .apply_delta(&delta)
-            .map_err(|err: DeltaError| MemoryEngineError::Commit(
-                CommitError::TlogWrite(
-                    std::io::Error::new(std::io::ErrorKind::Other, err.to_string()),
-                ),
-            ))?;
+impl Kernel {
+    pub fn commit_delta(&self, admission: &AdmissionProof, delta_hash: &StateHash) -> Result<CommitProof, KernelError> {
+        let delta = self.fetch_delta_by_hash(delta_hash).ok_or(KernelError::DeltaNotFound)?;
+        self.state.write().apply_delta(&delta).map_err(|err: DeltaError| KernelError::Commit(CommitError::TlogWrite(std::io::Error::new(std::io::ErrorKind::Other, err.to_string()))))?;
         self.state.write().page_store.flush().ok();
         self.epoch.increment();
-        Ok(CommitProof {
-            admission_proof_hash: admission.hash(),
-            delta_hash: *delta_hash,
-            state_hash: self.state.read().root_hash(),
-        })
+        Ok(CommitProof { admission_proof_hash: admission.hash(), delta_hash: *delta_hash, state_hash: self.state.read().root_hash() })
     }
 }
