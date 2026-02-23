5986974257e482148555a32f9d3dbe3c7a151974
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-23 04:48:01 -0500

    auto

--- capture/src/lib.rs
+++ capture/src/lib.rs
@@ -1,18 +1,1 @@
-#![cfg_attr(feature = "rustc_frontend", feature(rustc_private))]
-#![allow(dead_code)]
-
-#[cfg(feature = "rustc_frontend")]
-extern crate rustc_driver;
-#[cfg(feature = "rustc_frontend")]
-extern crate rustc_hir;
-#[cfg(feature = "rustc_frontend")]
-extern crate rustc_interface;
-#[cfg(feature = "rustc_frontend")]
-extern crate rustc_middle;
-#[cfg(feature = "rustc_frontend")]
-extern crate rustc_span;
-
-#[path = "shared.rs"]
-mod shared;
-
-pub use shared::*;
+// placeholder
