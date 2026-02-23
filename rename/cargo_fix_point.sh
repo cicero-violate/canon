0cfedeb511861323a76ccf4b885d6ab463b78649
Cicero Bugash <cicero.bugash@gmail.com>
2026-02-22 23:19:41 -0500

    auto

--- rename/cargo_fix_point.sh
+++ rename/cargo_fix_point.sh
@@ -4,4 +4,7 @@ rm -f .rename/* 2>/dev/null
 rm -rf fix_point/src/*
 find .. -name "state.tlog" -o -name "state.log" -o -name "*.graph.log" | head -20
 
-cargo run --example nodeop_movesymbol
+# cargo run --example nodeop_movesymbol
+RENAME_DEBUG_PLAN=1 cargo run --example nodeop_movesymbol 2>&1 \
+  | grep "AliasGraph\|NodeId\|dedup\|top-level" \
+  | head -40
